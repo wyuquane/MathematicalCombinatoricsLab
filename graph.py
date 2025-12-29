9082cdcacb5ea655ad8e979a73f955ca94ff82a2
Huy Hoang <lehuyhoang159753@gmail.com>
2025-12-29 23:38:15 +0700

    đi ngủ dù vẫn chưa fig the buck

--- graph.py
+++ graph.py
@@ -21,7 +21,6 @@ def find_circuit_dfs(graph: list[list[int]], source: int = 0) -> list[int] | Non
     n = len(graph)
     circuit = [source]
     visited_v = [False] * n
-    visited_e = [[False] * n for _ in range(n)]
 
     stack = [source]
     visited_v[source] = True
@@ -31,7 +30,8 @@ def find_circuit_dfs(graph: list[list[int]], source: int = 0) -> list[int] | Non
         be_popped = True
 
         for v in graph[u]:
-            if v == source and not visited_e[u][v]:
+            print(v, u, graph[u])
+            if v == source and len(circuit) > 2:
                 return circuit + [v]
             if not visited_v[v]:
                 be_popped = False
@@ -40,18 +40,33 @@ def find_circuit_dfs(graph: list[list[int]], source: int = 0) -> list[int] | Non
         if be_popped:
             stack.pop()
         else:
-            print(v)
             stack.append(v)
             circuit.append(v)
             visited_v[v] = True
-            visited_e[u][v] = visited_e[v][u] = True
 
     return None
 
+def is_valid_circuit(graph: list[list[int]], circuit):
+    if circuit[0] != circuit[-1]:
+        return False
+    n = len(graph)
+    visited_e = [[False] * n for _ in range(n)]
+    for i in range(len(circuit) - 1):
+        if circuit[i + 1] not in graph[circuit[i]]:
+            print("khong co canh")
+            return False
+        if visited_e[circuit[i]][circuit[i + 1]]:
+            print("lap canh")
+            return False
+        visited_e[circuit[i]][circuit[i + 1]] = visited_e[circuit[i]][circuit[i + 1]] = True
+    return True
 
 
 if __name__ == "__main__":
     adjacency_matrix = load_graph()
     adjacency_list = to_adjacency_list(adjacency_matrix)
-    print(adjacency_list)
-    print(find_circuit_dfs(adjacency_list, 0))
+    for i in range(len(adjacency_list)):
+        print(i, adjacency_list[i])
+    print("-------------")
+    circuit = find_circuit_dfs(adjacency_list, 6)
+    print(circuit)
