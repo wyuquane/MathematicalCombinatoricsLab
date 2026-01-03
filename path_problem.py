f308ff2d9006559389a602a6b2d31b18bfb4b4a9
Huy Hoang <lehuyhoang159753@gmail.com>
2026-01-03 21:46:28 +0700

    bfs, đếm số liên thông, bfs shortest path

--- path_problem.py
+++ path_problem.py
@@ -69,10 +69,85 @@ def dfs(graph: list[list[tuple[int, int]]], start: int) -> list[int]:
 
     return list_node_dfs
 
+def bfs(graph: list[list[tuple[int, int]]], start: int) -> list[int]:
+    """
+    iterative breadth first search
+    :param graph: adjacency list
+    :param start: node that bfs begin
+    :return: vertices with bfs ordering
+    """
+    n = len(graph)
+    visited = [False] * n
+    list_node_bfs = [start]
+    queue = [start]
+    visited[start] = True
+
+    while queue:
+        u = queue.pop(0)
+
+        for v in graph[u]:
+            if not visited[v[0]]:
+                queue.append(v[0])
+                visited[v[0]] = True
+                list_node_bfs.append(v[0])
+
+    return list_node_bfs
+
+def count_components(graph: list[list[tuple[int, int]]]) -> int:
+    n = len(graph)
+    result = 0
+    visited = [False] * n
+    def local_dfs(u):
+        visited[u] = True
+        for v, w in graph[u]:
+            if not visited[v]:
+                local_dfs(v)
+    for u in range(n):
+        if not visited[u]:
+            local_dfs(u)
+            result += 1
+
+    return result
+
+def shortest_path_bfs(graph: list[list[tuple[int, int]]], start: int, end: int) -> list[int]:
+    """
+    :param graph: adjacency list
+    :param start: source
+    :param end: destination
+    :return: the path with the fewest edges
+    """
+    n = len(graph)
+    dist = [float("inf")] * n
+    parent = [-1] * n
+    queue = [start]
+
+    dist[start] = 0
+
+    while queue:
+        u = queue.pop(0)
+
+        for v, w in graph[u]:
+            if dist[v] == float("inf"):
+                dist[v] = dist[u] + 1
+                parent[v] = u
+                queue.append(v)
+
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+
+    return path[::-1]
+
+
+
 
 if __name__ == "__main__":
     adjacency_list = load_file()
     for node in adjacency_list:
         print(node)
-    nodes = dfs(adjacency_list, 2)
-    print(nodes)
+    print("---------")
+    dfs_ordering = dfs(adjacency_list, 0)
+    bfs_ordering = bfs(adjacency_list, 0)
+    print(dfs_ordering, bfs_ordering)
+    print(count_components(adjacency_list))
+    print(shortest_path_bfs(adjacency_list, 0, 9))
