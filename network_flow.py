2a9207f1dc1aae3335cf580c39b92e704dfef378
Huy Hoang <lehuyhoang159753@gmail.com>
2026-01-04 16:31:46 +0700

    thêm tính toán sức chứa của mặt cắt, đồ thị thặng dư

--- network_flow.py
+++ network_flow.py
@@ -2,13 +2,31 @@ import numpy as np
 
 
 def load_graph(filename: str = "directed_weighted_graph.txt") -> list[list[int]]:
+    """
+    :param filename:
+    :return: adjacency matrix
+    """
     graph = []
     file_in = open(filename)
     for line in file_in:
         graph.append(list(map(int, line.strip().split())))
     return graph
 
-def print_all_cut(graph: list[list[int]]) -> None:
+def compute_s_t_cut(graph: list[list[int]], s: list[int], t: list[int]) -> int:
+    """
+    :param graph: adjacency matrix
+    :param s:
+    :param t:
+    :return: capacity of the cut
+    """
+    result = 0
+    for u in s:
+        for v in t:
+            result += graph[u][v]
+
+    return result
+
+def print_all_cuts(graph: list[list[int]]) -> None:
     n = len(graph)
     in_s = [False] * n
     in_s[0]  = True
@@ -22,7 +40,8 @@ def print_all_cut(graph: list[list[int]]) -> None:
                     s.append(i)
                 else:
                     t.append(i)
-            print(s, t)
+            capacity = compute_s_t_cut(graph, s, t)
+            print(s, t, capacity)
             return
 
         in_s[index] = 0
@@ -33,8 +52,20 @@ def print_all_cut(graph: list[list[int]]) -> None:
     backtracking(1)
 
 
+def construct_residual_graph(capacity: list[list[int]], flow: list[list[int]]) -> residual_graph:
+    n = len(graph)
+    residual_graph = [[0] * n for _ in range(n)]
+
+    for u in range(n):
+        for v in range(n):
+            residual_graph[u][v] += capacity[u][v] - flow[u][v]
+            residual_graph[v][u] += flow[u][v]
+
+    return residual_graph
+
+
 
 if __name__ == "__main__":
     adjacency_matrix = load_graph()
     print(np.array(adjacency_matrix))
-    print_all_cut(adjacency_matrix)
+    print_all_cuts(adjacency_matrix)
